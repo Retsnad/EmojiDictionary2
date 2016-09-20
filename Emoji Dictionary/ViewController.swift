b0103d026723ad26abdda0d25cc9334c0c869bea
YoderDaniel <danielbyoder@gmail.com>
2016-09-20 11:34:02 +0900

    made emoji table

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -1,20 +1,41 @@
-//
-//  ViewController.swift
-//  Emoji Dictionary
-//
-//  Created by YoderDaniel on 9/20/16.
-//  Copyright © 2016 YoderDaniel. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var EmojiTableView: UITableView!
+    
+    var emojis = ["😁","😎","😚","😡","🤔","😇","😆","😔","😨","💩"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        EmojiTableView.dataSource = self
+        EmojiTableView.delegate = self
     }
 
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
